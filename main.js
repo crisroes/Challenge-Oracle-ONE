afecf9a4c6be1ee8a5958379341d29b196022eb3
crisroes <cristian.roescobar@gmail.com>
2023-04-12 01:40:14 -0500

    fix: error el texto encriptado se acumula

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var vocales = ["a", "e", "i", "o", "u"];
 var code = ["ai", "enter", "imes", "ober", "ufat"];
-var textoEncriptado = "";
 
 function encriptarTexto() {
-  var textoEntrada = document.getElementById("textoEntrada").value;
-  var t = textoEntrada.replace("a", "ai");
-  for (var x = 0; x < textoEntrada.length; x++) {
+  var textoEncriptado = "";
+  let textoEntrada = document.getElementById("textoEntrada").value;
+  for (let x = 0; x < textoEntrada.length; x++) {
     if (buscarVocal(textoEntrada[x])) {
       textoEncriptado = textoEncriptado + buscarVocal(textoEntrada[x]);
     } else if (textoEntrada[x] == " ") {
@@ -14,11 +13,9 @@ function encriptarTexto() {
       textoEncriptado = textoEncriptado + textoEntrada[x];
     }
   }
-
-  console.log(textoEncriptado);
 }
 function buscarVocal(elemento) {
-  for (var y = 0; y < 5; y++) {
+  for (let y = 0; y < 5; y++) {
     if (elemento == vocales[y]) {
       return code[y];
       break;
